8c2aa401d7cc5cce80a0649590a75dc305352902
img10 <img10@img10noMacBook-Pro.local>
2024-10-20 19:37:37 +0900

    first commit

---
+++ GithubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubApp
+//
+//  Created by img10 on 2024/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
